5deba69fc92e59e40421bae68f3d979c0ff918da
ftorradog05 <ftorradog05@iescastelar.com>
2026-01-12 13:43:01 +0100

    Creación del proyecto

---
+++ src/main/java/com/krasky/krasky/KraSkyApplication.java
@@ -0,0 +1,13 @@
+package com.krasky.krasky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KraSkyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KraSkyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krasky/krasky/KraSkyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krasky.krasky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KraSkyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
